c4e3507ee0f49956401cceee5574b783df2bfe5b
TTV-msVenom <63176268+zarlicho@users.noreply.github.com>
2021-12-02 18:26:04 +0700

    Update autoReply.py

--- youtubeRply/autoReply.py
+++ youtubeRply/autoReply.py
@@ -13,27 +13,19 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=opt)
 
 
 
-def channel(yt, komen, jumlah, waktu, old):
+def channel(yt, komen, jumlah, waktu):
     driver.get(yt)
-    for x in range(1, jumlah):
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[1]/ytd-comments-header-renderer/div[1]/span/yt-sort-filter-sub-menu-renderer/yt-dropdown-menu"))).click()
-        if old == "lama":
-            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='menu']/a[1]/tp-yt-paper-item/tp-yt-paper-item-body/div[1]"))).click()
-        elif old == "baru":
-            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='menu']/a[2]/tp-yt-paper-item/tp-yt-paper-item-body/div[1]"))).click()
-
-        rpl = "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[%s]/ytd-comment-renderer/div[3]/div[2]/ytd-comment-action-buttons-renderer/div[1]/div[4]"%(x)
-        bls = "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[%s]/ytd-comment-renderer/div[3]/div[2]/ytd-comment-action-buttons-renderer/div[2]/ytd-comment-reply-dialog-renderer/ytd-commentbox/div[2]/div/div[2]/tp-yt-paper-input-container/div[2]/div/div[1]/ytd-emoji-input/yt-user-mention-autosuggest-input/yt-formatted-string/div"%(x)
-        btn = "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[%s]/ytd-comment-renderer/div[3]/div[2]/ytd-comment-action-buttons-renderer/div[2]/ytd-comment-reply-dialog-renderer/ytd-commentbox/div[2]/div/div[4]/div[5]/ytd-button-renderer[2]/a/tp-yt-paper-button/yt-formatted-string"%(x)
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, rpl))).click()  
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, bls))).send_keys(komen)
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, btn))).click()
-        time.sleep(waktu)  
+    rpl = "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[%s]/ytd-comment-renderer/div[3]/div[2]/ytd-comment-action-buttons-renderer/div[1]/div[4]"%(x)
+    bls = "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[%s]/ytd-comment-renderer/div[3]/div[2]/ytd-comment-action-buttons-renderer/div[2]/ytd-comment-reply-dialog-renderer/ytd-commentbox/div[2]/div/div[2]/tp-yt-paper-input-container/div[2]/div/div[1]/ytd-emoji-input/yt-user-mention-autosuggest-input/yt-formatted-string/div"%(x)
+    btn = "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/ytd-comments/ytd-item-section-renderer/div[3]/ytd-comment-thread-renderer[%s]/ytd-comment-renderer/div[3]/div[2]/ytd-comment-action-buttons-renderer/div[2]/ytd-comment-reply-dialog-renderer/ytd-commentbox/div[2]/div/div[4]/div[5]/ytd-button-renderer[2]/a/tp-yt-paper-button/yt-formatted-string"%(x)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, rpl))).click()  
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, bls))).send_keys(komen)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, btn))).click()
+    time.sleep(waktu)  
     
 
 ch = input("masukan link video dan komen: ")
 kom = input("masukan komen: ")
 jmlh = int(input("masukan jumlah: "))
 wkt = int(input("masukan delay untuk komen: "))
-menu = input("old coment or new comment: ")
-channel(ch, kom, jmlh, wkt, menu)
+channel(ch, kom, jmlh, wkt)
